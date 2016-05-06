4ba37c6fe8a5204cdcd291f8319670ad64673dfe
Garrison Shea <garrisonshea@gmail.com>
2016-05-05 19:18:08 -0500

    refreshGrid function to undo sketches

--- js/sketchpad.js
+++ js/sketchpad.js
@@ -1,21 +1,33 @@
 // This Sketchpad application will create a 960px grid that reacts to the user's mouse 
 // hovering over the indiviual squares. 
-
+$(document).ready(function() {
+	$(".sketch").mouseenter(function() {
+		$(this).css("background", "#EB7F00");
+	});
+	$("#default").click(function() {
+		$(".sketch").css("background", "#225378");
+	})
+});
 
 // This function will run on load and will create the default sketchpad
 function createSketchpad(width) {
 	var squareWidth = Math.floor(960 / width);
 	for (var j = 0; j < width; j++) {
 		for (var i = 0; i < width; i++) {
-			var div = document.createElement('div');
-			div.setAttribute('class', 'sketch');
+			var div = document.createElement("div");
+			div.setAttribute("class", "sketch");
 			document.getElementById("container").appendChild(div);
 		}
 	}
 	console.log(squareWidth);
-	$('.sketch').width(squareWidth);
-	$('.sketch').height($('.sketch').width());
+	$(".sketch").width(squareWidth);
+	$(".sketch").height($(".sketch").width());
 	// $('#container').height($('.sketch').width() * width);
+}
+
+//Sets all squares back to the blue, untouched color
+function clearGrid() {
+	$(".sketch").css("background", "#225378");
 }
 
 createSketchpad(16);
